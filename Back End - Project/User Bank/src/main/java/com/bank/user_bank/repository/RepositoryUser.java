c0b368ad748b2a53848cf244c624d38c4f6387a3
DANILO ALMEIDA DAMASCENO <danilo.engauto@gmail.com>
2023-11-27 10:53:33 -0300

    🏷️ add Varias Classes Iniciais

--- Back End - Project/User Bank/src/main/java/com/bank/user_bank/repository/RepositoryUser.java
+++
@@ -1,8 +0,0 @@
-package com.bank.user_bank.repository;
-
-import com.bank.user_bank.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RepositoryUser extends JpaRepository<User, Long> {
-
-}

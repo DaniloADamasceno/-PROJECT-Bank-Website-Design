9991af70384d79c3e188ba295573f039500d6e1d
DANILO ALMEIDA DAMASCENO <danilo.engauto@gmail.com>
2023-11-27 11:37:27 -0300

    📦 part 3 FINAL -> Add Classes iniciais

---
+++ Bank Softway/src/test/java/com/bank/softway/BankSoftwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.softway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSoftwayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
